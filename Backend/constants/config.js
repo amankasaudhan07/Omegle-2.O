b465b28a843f15b69e8a56c6ad199063da40849b
Aman <amankasaudhan07@gmail.com>
2024-10-15 22:33:20 +0530

    cors solved

--- Backend/constants/config.js
+++ Backend/constants/config.js
@@ -4,9 +4,7 @@ dotenv.config();
 
 const corsOptions = {
     origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      process.env.CLIENT_URL,
+     true,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
